aca7eb8095cb3386163498281df4f3e1f62d9b4f
mz <815390944@qq.com>
2022-12-01 12:04:45 +0800

    1

--- midterm_project/LinearRegress_model.py
+++ midterm_project/LinearRegress_model.py
@@ -5,10 +5,12 @@ class LinearRegress(nn.Module):
     def __init__(self, inputsize, outputsize):  # 输入输出均为一维的
         super(LinearRegress, self).__init__()
         self.Linear1 = nn.Linear(in_features=inputsize, out_features=outputsize)
+        self.sigmoid = nn.Sigmoid()
 
     # forward函数，依次并且输出
     def forward(self, x):
-        return self.Linear1(x)
+        res_out = self.sigmoid(self.Linear1(x))
+        return res_out
 
 
 if __name__ == '__main__':
